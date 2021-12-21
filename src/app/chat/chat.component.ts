01f59cdecc17553b44f286aabd214568dd1b2dc4
Miguel <miguelvad18@gmail.com>
2021-12-21 12:41:38 +0100

    chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,3 +1,4 @@
+import { ElementSchemaRegistry } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-
-  constructor() { }
+  mostrarChat = false;
+  usuariologueado: any;
+  nuevoMensaje: String= "";
+  mensajes: any = [{
+    emisor: "id",
+    texto: "hola que tal"
+  }, {
+    emisor: "id",
+    texto: "todo bien, vos?"
+  }, {
+    emisor: "id",
+    texto: "todo perfecto"
+  }, {
+    emisor: "id",
+    texto: "hola gente"
+  }, {
+    emisor: "id",
+    texto: "que onda"
+  },];
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.getUserLogged().subcribe(usuario => {
+      this.usuariologueado = usuario;
+    })
+  }
+  enviarMensaje(){
+    if (this.nuevoMensaje == "") return; 
+    console.log(this.nuevoMensaje);
+    let mensaje={
+      emisor: this.usuariologueado.uid,
+      texto: this.nuevoMensaje
+    }
+    this.mensajes.push(mensaje);
+
+    this.nuevoMensaje = "";
+
+    setTimeout(() => {
+      this.scrollToTheLastElementByClassName();
+    }, 10);
+    
   }
 
+  scrollToTheLastElementByClassName(){
+    let elements = document.getElementsByClassName('msj');
+    let ultimo:any = elements[elements.length - 1];
+    let toppos = ultimo.offsetTop;
+
+    //@ts-ignore
+    document.getElementById('contenedorDeMensajes')?.scrollTop = toppos;
+  }
 }
